1a7132f628f3d9b7978082aa5ad5b92a2fd6b1be
Lakshmi E <lakshmi.e@CELSYSWTC.IN>
2025-05-27 13:03:32 +0530

    Ceaser cipher is now completed as part 08 menu-driven-function-understanding.

---
+++ Day08/ceaserCipher.py
@@ -0,0 +1,40 @@
+proceed = 'start'
+print("Welcome to ceaser cipher. The process of encrypting the messages and decrypting them to make unintended person understanding the intent of the message")
+while(proceed in ('start', 'y')):
+    def ask_proceed():
+        proceed = input("\nEnter `y` to proceed or `n` to exit").strip().lower()
+        if(proceed == 'n'):
+            exit()
+        elif(proceed not in ('y')):
+            proceed = print("\n Invalid choice.", end=" ")
+            ask_proceed()
+    if(proceed != 'start'):
+        ask_proceed()
+    input_str = ""
+    turns = 0
+    def encrpt_decrypt(action, str, turns):
+        for i in str:
+            if (action == 'encrypt'):
+                print(chr(ord(i) + turns),end="")
+            elif (action == 'decrypt'):
+                print(chr(ord(i) - turns),end="")
+    def ask_action():
+        action = input("\nEnter `encrypt` or `decrypt` to start the process").strip().lower()
+        # print(action)
+        if(action not in ('encrypt','decrypt')):
+            ask_action()
+        return action
+    def ask_input():
+        global action
+        # action = ask_action()
+        global input_str, turns
+        input_str = input(f"Enter the text you need to {action}").strip()
+        turns = int(input(f"Enter the rotations for the {action}ion"))
+        
+    action = ask_action()
+    ask_input()
+    # input_str, turn = ask_input()
+    # print(type(input_str), input_str, turns , type(turns))
+    encrpt_decrypt(action, input_str, turns)
+    proceed = 'y'
+
