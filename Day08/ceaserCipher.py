9e7734c7cce9f0e09fca17f7bbb86f3b26775121
Lakshmi E <lakshmi.e@CELSYSWTC.IN>
2025-05-27 15:32:01 +0530

    Changed the logic for cipher insted of just ord and chr, it is now with index of alphabets array for more control over the logic.

--- Day08/ceaserCipher.py
+++ Day08/ceaserCipher.py
@@ -1,5 +1,9 @@
+alphabets = []
+for letter in range(ord('A'), ord('Z') + 1):
+    # print(chr(letter))
+    alphabets.append(chr(letter).lower())
 proceed = 'start'
-print("Welcome to ceaser cipher. The process of encrypting the messages and decrypting them to make unintended person understanding the intent of the message")
+print("Welcome to ceaser cipher", "The process of encrypting the messages and decrypting them", "to make unintended person understanding the intent of the message", sep ="\n")
 while(proceed in ('start', 'y')):
     def ask_proceed():
         proceed = input("\nEnter `y` to proceed or `n` to exit").strip().lower()
@@ -13,11 +17,20 @@ while(proceed in ('start', 'y')):
     input_str = ""
     turns = 0
     def encrpt_decrypt(action, str, turns):
-        for i in str:
+        for each_letter in str:
             if (action == 'encrypt'):
-                print(chr(ord(i) + turns),end="")
+                # print(each_letter in alphabets, each_letter)
+                if each_letter in alphabets:
+                    index = (alphabets.index(each_letter) + turns)
+                    order_value = 26 - index if(index >= 26) else index
+                    # print(index, order_value)
+                    print(alphabets[order_value], end="")
             elif (action == 'decrypt'):
-                print(chr(ord(i) - turns),end="")
+                if each_letter in alphabets:
+                    index = (alphabets.index(each_letter) - turns)
+                    order_value = 26 + index if(index < 0) else index
+                    # print(index, order_value)
+                    print(alphabets[order_value], end="")
     def ask_action():
         action = input("\nEnter `encrypt` or `decrypt` to start the process").strip().lower()
         # print(action)
